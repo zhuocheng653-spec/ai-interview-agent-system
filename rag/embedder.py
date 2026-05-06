03656fadf540105b15e9e4979d0cfc863f3fbb2a
zhuocheng653-spec <zhuocheng653@gmail.com>
2026-05-06 16:50:28 +0800

    Create embedder.py

---
+++ rag/embedder.py
@@ -0,0 +1,10 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+def get_embedding(text):
+    response = client.embeddings.create(
+        model="text-embedding-3-small",
+        input=text
+    )
+    return response.data[0].embedding
